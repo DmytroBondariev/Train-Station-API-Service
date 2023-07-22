6f352557fddd4d8cc4f67d7d8dad0f798107f3d6
Dmytry Bondariev <bondarievmd@gmail.com>
2023-07-22 23:57:22 +0300

    Added all models to admin interface

--- train_station/admin.py
+++ train_station/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from train_station.models import Station, Train, TrainType, Route, Journey, Order, Ticket
+
+admin.site.register(Station)
+admin.site.register(Train)
+admin.site.register(TrainType)
+admin.site.register(Route)
+admin.site.register(Journey)
+admin.site.register(Order)
+admin.site.register(Ticket)
